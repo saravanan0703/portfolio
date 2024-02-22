972121d9e571a04e05cf096b514442f34d8644a1
saravanan0703 <saravanan57157@gmail.com>
2024-02-22 12:46:05 +0530

    improved

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import './App.css';
 import Footer from './Components/footer';
 import Navbar from './Components/navbar';
 import Home from './Pages/home';
-
+import Welcome from './Components/welcome';
+import { useEffect, useState } from 'react';
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an asynchronous action (e.g., fetching data)
+    const fetchData = async () => {
+      // Replace this with your actual asynchronous action
+      await new Promise((resolve) => setTimeout(resolve, 7500));
+      setLoading(false); // Set loading to false when your action is done
+    };
+
+    fetchData();
+  }, []);
+  
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      <Footer />
+     {loading ? <Welcome /> : (
+        <>
+          <Navbar />
+          <Home />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
